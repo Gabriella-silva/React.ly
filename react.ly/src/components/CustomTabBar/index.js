ac4f76e910ac4c7e87ec4c8dba8d305919325a4c
gabriella-silva <gabriellaferreira534@gmail.com>
2025-03-07 21:35:07 -0300

    <3

--- react.ly/src/components/CustomTabBar/index.js
+++ react.ly/src/components/CustomTabBar/index.js
@@ -1,9 +1,36 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 
-export default function CustomTabBar() {
+export default function CustomTabBar( state, descriptors, navigation) {
   return(
-     <View >
-        <Text>TESTE</Text>
+     <View style={styles.container} >
+        <View style={styles.content}>
+            {state.routes.map((route, index)=>{
+                const {options} = descriptors[route.key];
+
+
+                const isFocused = state.index === index;
+
+                const onPress = () =>{
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
+
+                    if(!isFocused && !event.defaultPrevented){
+                        navigation.navigate(route.name);
+                    }
+                };
+
+                const onLongPress = () =>{
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
+            })}
+              <Text>TESTE</Text>
+        </View>
     </View>
   );
 }
